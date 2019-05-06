87e32410b80b6e67de550a1d66b88aba8b828d15
Drashti Mehta <drashtii.mehta@gmail.com>
2019-05-05 20:10:31 -0400

    commit final

---
+++ Assets/Clicktomove.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Clicktomove : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public Animator anim;
+
+    public float startTime, endTime;
+
+    RaycastHit hit;
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        startTime = 0.0f;
+        endTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+    
+            startTime = Time.time;
+
+        if (Input.GetMouseButtonUp(0))
+
+            endTime = Time.time;
+
+
+        if (endTime - startTime > 0)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+
+            if (endTime - startTime < 0.5f)
+            {
+                if (agent.remainingDistance > agent.stoppingDistance)
+                {
+                    anim.SetInteger("condition", 1);
+                }
+                else
+                {
+                    anim.SetInteger("condition", 0);
+                }
+            }
+
+            else if (endTime - startTime > 0.5f)
+            {
+                if (agent.remainingDistance > agent.stoppingDistance)
+                {
+                    anim.SetInteger("condition", 2);
+                }
+                else
+                {
+                    anim.SetInteger("condition", 0);
+                }
+            }
+
+
+        }
+
+        startTime = 0;
+        endTime = 0;
+    }
+}
--- Assets/Female_Controller.cs
+++ Assets/Female_Controller.cs
@@ -1,76 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Female_Controller : MonoBehaviour
 {
-    float walkspeed = 2;
-    float runspeed = 4;
-    float bwalkspeed = 3;
-    float jumpspeed = 2;
+    [SerializeField]
+
+    Vector3 forward, right;
+
+    float speed;
     float rotSpeed = 80;
     float rot = 0f;
     float gravity = 5;
 
    private Vector3 moveDir = Vector3.zero;
 
+
     CharacterController controller;
     Animator anim;
 
+
     void Start()
     {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
 
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+       
     }
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            anim.SetInteger("condition", 1);
+            speed = 2;
+            moveDir = new Vector3(0, 0, 1);
+            moveDir *= speed;
+            moveDir = transform.TransformDirection(moveDir);
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                anim.SetInteger("condition", 1);
-                moveDir = new Vector3(0, 0, 1);
-                moveDir *= walkspeed;
-                moveDir = transform.TransformDirection(moveDir);
+        }
 
-            }
+        if (Input.GetKey(KeyCode.B))
+        {
+            anim.SetInteger("condition", 1);
+            speed = 3;
+            moveDir = new Vector3(0, 0, -1);
+            moveDir *= speed;
+            moveDir = transform.TransformDirection(moveDir);
 
-            if (Input.GetKey(KeyCode.B))
-            {
-                anim.SetInteger("condition", 1);
-                moveDir = new Vector3(0, 0, -1);
-                moveDir *= bwalkspeed;
-                moveDir = transform.TransformDirection(moveDir);
+        }
 
-            }
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetInteger("condition", 2);
+            speed = 4;
+            moveDir = new Vector3(0, 0, 1);
+            moveDir *= speed;
+            moveDir = transform.TransformDirection(moveDir);
 
-            if (Input.GetKey(KeyCode.R))
-            {
-                anim.SetInteger("condition", 2);
-                moveDir = new Vector3(0, 0, 1);
-                moveDir *= runspeed;
-                moveDir = transform.TransformDirection(moveDir);
+        }
 
-            }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                anim.SetInteger("condition", 3);
-                moveDir = new Vector3(0, 4, 0);
-                moveDir *= jumpspeed;
-                moveDir = transform.TransformDirection(moveDir);
-            }
-
-            if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.R)||Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.B))
-            {
-                anim.SetInteger("condition", 0);
-                moveDir = new Vector3(0, 0, 0);
-            }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetInteger("condition", 3);
+            moveDir = new Vector3(0, 10, 0);
+            moveDir *= 1;
+            moveDir = transform.TransformDirection(moveDir);
+        }
 
         rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-        transform.eulerAngles = new Vector3(0,rot,0); 
+        transform.eulerAngles = new Vector3(0, rot, 0);
         moveDir.y -= gravity * Time.deltaTime;
         controller.Move(moveDir * Time.deltaTime);
 
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Move();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.R) || Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.B))
+        {
+            anim.SetInteger("condition", 0);
+            moveDir = new Vector3(0, 0, 0);
+        }
+
+        
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontalkey"), 0, Input.GetAxis("Verticalkey"));
+        Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis("Horizontalkey");
+        Vector3 upMovement = right * speed * Time.deltaTime * Input.GetAxis("Verticalkey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+       
     }
 }
